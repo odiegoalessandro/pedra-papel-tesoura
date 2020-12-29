5676bfa5b5aadfe9c10f606ec75d950336960121
Diego Alessandro da Cruz Martins <diegoalessandrodacruzmartins@gmail.com>
2020-12-28 23:22:21 -0300

    criando interação com a maquina

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const game = {
     machineScore: 0,
     humanScore: 0,
-    machineOptions: ['icon-paper.svg', 'icon-rock.svg' ,'icon-scissors.svg'],
+    options: ['icon-paper.svg', 'icon-rock.svg' ,'icon-scissors.svg'],
 
     /*
         PAPER = 1,
@@ -18,7 +18,12 @@ const game = {
     selectMachineOption: function(){
         let optionSelected = Math.floor(Math.random() * 3 + 1)
         let botCombat = document.querySelector(".botOption")
-        return botCombat.innerHTML = `<img src="./images/${this.machineOptions[optionSelected - 1]}">`
+        return botCombat.innerHTML = `<img class="bot" src="./images/${this.options[optionSelected - 1]}">`
+    },
+    selectHumanOption: function(option){
+        let optionSelected = option
+        let humanCombat = document.querySelector(".humanOption")
+        game.selectMachineOption()
+        return humanCombat.innerHTML = `<img class="human" src="./images/${this.options[optionSelected - 1]}">`
     }
-
 }
